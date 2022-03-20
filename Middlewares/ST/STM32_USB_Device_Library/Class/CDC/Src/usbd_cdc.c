d86f3d737d9915609f8364371a8600db209e7b50
rmh2020 <702435148@qq.com>
2022-03-20 13:45:53 +0800

    1

--- Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src/usbd_cdc.c
+++ Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src/usbd_cdc.c
@@ -58,7 +58,7 @@ EndBSPDependencies */
 /* Includes ------------------------------------------------------------------*/
 #include "usbd_cdc.h"
 #include "usbd_ctlreq.h"
-
+#include "stdint.h"
 
 /** @addtogroup STM32_USB_DEVICE_LIBRARY
   * @{
